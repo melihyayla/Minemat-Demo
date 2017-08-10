d8a289f11115c14ee842170b8fe9b916308025e1
melihyayla <melihyayla@gmail.com>
2017-08-10 21:09:32 +0300

    first commit

---
+++ Documents/Android workspace/Mdemo2/app/src/main/java/com/example/melih/mdemo2/ForgotMyPassword.java
@@ -0,0 +1,13 @@
+package com.example.melih.mdemo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForgotMyPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_my_password);
+    }
+}
